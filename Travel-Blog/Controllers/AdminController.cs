21c572b42a14cb2d6ef531f96e2555caeb8047e9
Furkan <frknglww@gmail.com>
2026-07-05 20:31:29 +0300

    Admin tarafında kullancıı ayarları yapıldı ve aynı şekilde backend hataları düzeltildi yine aynı şekilde SQL dosyası güncellendi.

--- Travel-Blog/Controllers/AdminController.cs
+++ Travel-Blog/Controllers/AdminController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using Travel_Blog.Models;
 
 namespace Travel_Blog.Controllers
@@ -29,6 +31,68 @@ namespace Travel_Blog.Controllers
             data.AdminList = await db.TBLADMIN.Where(x => x.STATUS == true).ToListAsync();
             data.BlogsList = await db.TBLBLOGS.Where(x => x.STATUS == true).ToListAsync();
             return View(data);
+        }
+        [HttpGet]
+        public ActionResult UserSetting()
+        {
+            string currentUsername = User.Identity.Name;
+            var user = db.TBLADMIN.FirstOrDefault(x => x.USERNAME == currentUsername);
+            return View(user);
+        }
+
+        // --- 2. POST: AYARLARI GÜNCELLEME ---
+        [HttpPost]
+        public ActionResult UserSettingUpdate(TBLADMIN p, HttpPostedFileBase ImageFile)
+        {
+            var user = db.TBLADMIN.Find(p.ID);
+
+            if (user != null)
+            {
+                // Resim Yükleme İşlemi
+                if (ImageFile != null && ImageFile.ContentLength > 0)
+                {
+                    string fileName = Path.GetFileName(ImageFile.FileName);
+                    string path = Path.Combine(Server.MapPath("~/webimage"), fileName);
+                    ImageFile.SaveAs(path);
+
+                    user.IMAGE = fileName; // Sadece resim adı veritabanına yazılır
+                }
+
+                string oldUsername = user.USERNAME;
+
+                // Diğer Verileri Aktarma
+                user.NAME_AND_SURNAME = p.NAME_AND_SURNAME;
+                user.USERNAME = p.USERNAME;
+                user.EMAIL = p.EMAIL;
+                user.PASSWORD = p.PASSWORD;
+                user.JOB = p.JOB;
+                user.DESCRIPTION = p.DESCRIPTION;
+                user.TWITTER = p.TWITTER;
+                user.INSTAGRAM = p.INSTAGRAM;
+                user.LINKEDIN = p.LINKEDIN;
+
+                if (user.STATUS == false)
+                {
+                    user.STATUS = true;
+                }
+                else
+                {
+                    user.STATUS = false;
+                }
+
+                db.SaveChanges();
+
+                if (oldUsername != p.USERNAME)
+                {
+                    FormsAuthentication.SetAuthCookie(p.USERNAME, false);
+                }
+
+                TempData["UpdateSuccess"] = "Profil bilgileriniz başarıyla güncellendi.";
+                return RedirectToAction("UserSetting");
+            }
+
+            TempData["UpdateError"] = "Güncelleme sırasında bir hata oluştu.";
+            return RedirectToAction("UserSetting");
         }
     }
 }
